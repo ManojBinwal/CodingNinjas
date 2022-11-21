75e7eb2f42820a4744c2e87c9b945c41d503f670
Manoj Binwal <mail.manojbinwal@gmail.com>
2022-11-21 05:48:31 +0530

    SquarePatern java

---
+++ src/Patterns/SquarePatter.java
@@ -0,0 +1,17 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class SquarePatter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j =0; j<n; j++) {
+                System.out.print(n);
+            }
+            System.out.println();
+        }
+
+    }
+}
