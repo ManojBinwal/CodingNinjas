166c236e330e07bcb75effd0e39aadd52d5b83c7
Manoj Binwal <mail.manojbinwal@gmail.com>
2022-12-22 20:09:35 +0530

    java

--- src/LinkedList/LinkedListDemo.java
+++ src/LinkedList/LinkedListDemo.java
@@ -1,5 +1,5 @@
 package LinkedList;
-
+import java.util.*;
 import java.util.Scanner;
 
 class Node<T>{
@@ -23,29 +23,60 @@ public class LinkedListDemo {
 
     public static void main(String[] args) {
         // Create a new Node object with a value of 6 and assign it to the root variable
-        Node<Integer> root = new Node<>(6);
+        Node<Integer>root = new Node(6);
 
         // Add nodes with values 7, 8, and 9 to the end of the linked list
+
         root = add(7, root);
         root = add(8, root);
-        root = add(10,root);
+        root = add(9,root);
+        root = add(10, root);
         root = add(9, root);
+        root = add(8, root);
+        root = add(7,root);
+        root = add(6, root);
+
 
 //        root = input(root);
 //        root = inputLL(root);
 //        Print the values of the nodes in the linked list
-
+//
 //        root = insert(root, 3, 10);
 //        printIterative(root);
-
+//
 //        root=delete(root,5);
 //        printIterative(root);
-
+//
 //        Print the size of the linked list
 //        System.out.println(size(root));
-
+//
 //        // Print the value of the second element in the linked list
 //        System.out.println(get(root, 2));
+
+//        int index = getIndex(root,9);
+//        System.out.println(index);
+
+//        root=AppendLastNodeToFirst(root,0);
+//        printIterative(root);
+
+        //root = removeDuplicates(root);
+//        boolean contains = contains(root,10);
+//        System.out.println(contains);
+
+//        Node<Integer>newRoot=removeDuplicates(root);
+//        printIterative(newRoot);
+
+//        Node<Integer> newRoot = reverseLinkedList(root);
+//        printIterative(newRoot);
+
+//        printReverseRecursive(root);
+
+        boolean isPal = isPalindrome(root);
+        System.out.println(isPal);
+
+
+
+
 
 
 /*                                                                                                          /
@@ -53,6 +84,187 @@ public class LinkedListDemo {
  *                                   MAIN METHOD ENDS                                                        /
  * ----------------------------------------------------------------------------------------------------------/
  */
+    }
+
+    public static boolean isPalindrome(Node<Integer> head) {
+        //Your code goes here
+
+        Node<Integer> node=head;
+        ArrayList<Integer> arr = new ArrayList<>();
+        while (node!=null)
+        {
+            arr.add(node.data);
+            node=node.next;
+        }
+        int start=0, end=arr.size()-1;
+        while(start<end)
+        {
+            if (arr.get(start)!=arr.get(end))
+            {
+                return false;
+            }
+            else
+            {
+                start=start+1;
+                end=end-1;
+            }
+        }
+
+        return true;
+    }
+
+    public static void printReverseRecursive(Node<Integer> root) {
+        //Your code goes here
+        Node<Integer> smallOutput=printReverseRecursion(root);
+    }
+
+    public static Node<Integer> printReverseRecursion(Node<Integer> node)
+    {
+        if (node==null)
+            return node;
+        Node<Integer> smallOutput=printReverseRecursion(node.next);
+        System.out.print(node.data+" ");
+        return smallOutput;
+
+    }
+
+
+    private static Node<Integer> reverseLinkedList(Node<Integer> root) {
+        if(root==null){
+            return root;
+        }
+        ArrayList<Integer>temp = new ArrayList();
+        temp.add(root.data);
+        Node<Integer> current = root;
+        while(current.next!=null){
+            current=current.next;
+            temp.add(current.data);
+
+        }
+//        System.out.println(temp);
+        Node<Integer> newNode = new Node(temp.get(temp.size()-1));
+        int n = temp.size()-2;
+        while(n>=0){
+            newNode = add(temp.get(n),newNode);
+            n--;
+        }
+        return newNode;
+
+    }
+
+    private static boolean contains(Node<Integer> root,int n) {
+        // Check if root is null
+        if(root==null){
+            // If so, return false to indicate that the element was not found
+            return false;
+        }
+        // Check if the data in the root node is the element we are searching for
+        if(root.data==n){
+            // If so, return true if element is found
+            return true;
+        }
+        else{
+            // Initialize a counter to 1
+            int count = 1;
+            // Initialize a current node to root
+            Node<Integer> current = root;
+            // Iterate through the list until the end is reached or the element is found
+            while(current.next!=null){
+                current = current.next;
+                // Check if the current element is the one we are searching for
+                if(current.data==n){
+                    // If so, return true
+                    return true;
+                }
+                // Increment the counter
+                count++;
+            }
+        }
+        // If the element was not found, return false
+        return false;
+    }
+
+    private static Node<Integer> removeDuplicates(Node<Integer> root) {
+        if(root==null || root.next == null){
+            return root;
+        }
+        Node<Integer> newRoot = new Node(root.data);
+        while(root.next!=null){
+
+            root=root.next;
+            if(!contains(newRoot, root.data)){
+                newRoot=add(root.data,newRoot);
+            }
+        }
+
+        return newRoot;
+    }
+
+    private static Node<Integer> AppendLastNodeToFirst(Node<Integer> root,int pos) {
+        // Return the root node if pos is 0 or greater than or equal to the size of the linked list
+        if(pos==0||pos>=size(root)){
+            return root;
+        }
+
+        // Initialize variables to traverse the linked list and find the node to be moved
+        int size = size(root);
+        Node<Integer>current = root;
+        int i=0;
+        // Traverse the linked list to the node before the node to be moved
+        while(i<size-pos-1){
+            current = current.next;
+            i++;
+        }
+        // Save a reference to the node to be moved
+        Node<Integer> temp = current.next;
+        // Set the next node of the current node to null, effectively removing the node to be moved from the linked list
+        current.next=null;
+        // Set the new root of the linked list to be the node that was removed
+        Node<Integer>newRoot=temp;
+        // Set the current node to the new root of the linked list
+        current =temp;
+        // Traverse the linked list to the last node
+        while(current.next!=null){
+            current=current.next;
+        }
+        // Append the original root node to the end of the linked list
+        current.next=root;
+        // Return the new root of the linked list
+        return newRoot;
+    }
+
+
+    //method to get the index of specific element in the linkedlist
+    private static int getIndex(Node<Integer> root, int n) {
+        // Check if root is null
+        if(root==null){
+            // If so, return -1 to indicate that the element was not found
+            return -1;
+        }
+        // Check if the data in the root node is the element we are searching for
+        if(root.data==n){
+            // If so, return 0 as the index
+            return 0;
+        }
+        else{
+            // Initialize a counter to 1
+            int count = 1;
+            // Initialize a current node to root
+            Node<Integer> current = root;
+            // Iterate through the list until the end is reached or the element is found
+            while(current.next!=null){
+                current = current.next;
+                // Check if the current element is the one we are searching for
+                if(current.data==n){
+                    // If so, return the index
+                    return count;
+                }
+                // Increment the counter
+                count++;
+            }
+        }
+        // If the element was not found, return -1
+        return -1;
     }
 
     private static Node<Integer> delete(Node<Integer> root, int pos) {
@@ -89,7 +301,7 @@ public class LinkedListDemo {
 
     private static Node<Integer> insert(Node<Integer> root, int pos, int data) {
         // Create a new node with the given data
-        Node<Integer> newNode = new Node<>(data);
+        Node<Integer> newNode = new Node(data);
 
         // If the root is null, make the new node the root
         if (root == null) {
@@ -173,7 +385,7 @@ public class LinkedListDemo {
         int data = sc.nextInt();
 
         while (data != -1) {
-            Node<Integer> newNode = new Node<>(data);
+           Node<Integer> newNode = new Node(data);
             if (root == null) {
                 root = newNode;
                 last = newNode;
@@ -188,7 +400,6 @@ public class LinkedListDemo {
 
         return root;
     }
-
 
 
     // Method that returns the value of the ith element in a linked list
@@ -260,7 +471,7 @@ public class LinkedListDemo {
 
     public static Node<Integer> add(int data, Node root) {
         // Create a new Node object with the given data
-        Node<Integer> newNode = new Node<>(data);
+        Node<Integer> newNode = new Node(data);
         // If the linked list is empty, return the new Node object
         if (root == null) {
             return newNode;
